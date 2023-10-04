4e85ddf70050c9c84f841ae6271e55c82fdfa0d1
Andy Yu <andyyu2004@gmail.com>
2023-10-04 20:39:49 +1300

    use a more reliable algorithm for locating the expectation string
    Previously we would scan the line for the first quote character but this is easily broken.
    
    Now we parse out the string so it should be very reliable.

--- testdata/example_test.go
+++ testdata/example_test.go
@@ -7,12 +7,19 @@ import (
 )
 
 func TestExample(t *testing.T) {
-	expect.Expect(t, `foo`)
-	expect.Expect(t, ``)
-	expect.Expect(t, "double quoted string that can have ` in it")
-	expect.Expect(t, "escaped double quoted \"hi\" string")
-	expect.Expect(t, `backslash \ ignored in raw\`)
-	expect.Expect(t, `some
+	expect.Expect(`foo`)
+	expect.Expect(``)
+	expect.Expect("double quoted string that can have ` in it")
+	expect.Expect("escaped double quoted \"hi\" string")
+	expect.Expect(`backslash \ ignored in raw\`)
+	expect.Expect(`some
 multiline
 string`)
+
+	check := func(input string, expectation expect.Expectation) {
+		expectation.AssertEqual(t, input)
+	}
+
+	check(`test that the locate still works when there 
+is an unrelated backtick on the same line`, expect.Expect(`expected`))
 }
